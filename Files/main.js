43418f58b351df80274329164c62e6e00fd29240
Alex <45456278+pan-narrans@users.noreply.github.com>
2022-10-05 19:17:24 +0200

    Basic functionality done.
    All exercises are working.
    Presentation is a WIP.

--- Files/main.js
+++ Files/main.js
@@ -1,7 +1,8 @@
 
 //#region Reyes Magos
 const inReyesMagos = document.getElementById("inReyesMagos");
-const buReyesMagos = document.getElementById("buReyesMagos");
+const butReyesMagos = document.getElementById("butReyesMagos");
+const outReyesMagos = document.getElementById("outReyesMagos");
 const reyesMagos = fecha => {
   const fechaReyes = new Date();
   let tiempo = 0;
@@ -17,22 +18,27 @@ const reyesMagos = fecha => {
   return Math.floor(tiempo / (1000 * 60 * 60 * 24));
 };
 
-buReyesMagos.onclick = function () {
+butReyesMagos.onclick = function () {
   const tiempo = reyesMagos(new Date(inReyesMagos.value));
+  let message;
 
   if (tiempo < 0)
-    alert("Has viajado en el tiempo y se te han pasado los reyes.");
+    message = "Has viajado en el tiempo y se te han pasado los reyes.";
   else if (tiempo == 0)
-    alert("¡Mira debajo de el árbol que han llegado hoy!");
+    message = "¡Mira debajo de el árbol que han llegado hoy!";
   else
-    alert("Faltan " + tiempo + " días para que vengas los reyes.")
+    message = "Faltan " + tiempo + " días para que vengas los reyes.";
+
+  alert(message);
+  outReyesMagos.innerHTML = message;
 };
 //#endregion
 
 //#region Mostrar la fecha
-const buDate = document.getElementById("buDate");
+const butDate = document.getElementById("butDate");
+const outDate = document.getElementById("outDate");
 
-buDate.onclick = function () {
+butDate.onclick = function () {
   const fecha = new Date();
   const days = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"];
   const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
@@ -40,12 +46,14 @@ buDate.onclick = function () {
   let message = `Hoy es ${days[fecha.getDay()]}, ${fecha.getDate()} de ${months[fecha.getMonth()]} de ${fecha.getFullYear()} y son las ${fecha.getHours()}:${fecha.getMinutes()} horas.`
 
   alert(message);
+  outDate.innerHTML = message;
 }
 //#endregion
 
 //#region Círculo
 const inCircle = document.getElementById("inCircle");
-const buCircle = document.getElementById("buCircle");
+const butCircle = document.getElementById("butCircle");
+const outCircle = document.getElementById("outCircle");
 const calcularAreaCirculo = radio => {
   return Math.round(
     Math.PI * radio * radio * 100 + Number.EPSILON) / 100;
@@ -55,15 +63,73 @@ const calcularPeriCirculo = radio => {
     2 * Math.PI * radio * 100 + Number.EPSILON) / 100;
 };
 
-buCircle.onclick = function () {
-  const radio = inCircle.value;
+butCircle.onclick = function () {
+  const radio = parseInt(inCircle.value);
   const message = `El círculo de ${radio} uds. de radio tiene:
     - un perímetro de ${calcularPeriCirculo(radio)} uds.
     - un área de ${calcularAreaCirculo(radio)} uds.`;
   alert(message);
-}
+  outCircle.innerHTML = message.replace(/\n/g, "<br>");
+};
 //#endregion
 
+//#region Random number
 const inRandMin = document.getElementById("inRandMin");
 const inRandMax = document.getElementById("inRandMax");
-const buRand = document.getElementById("buRand");
+const butRand = document.getElementById("butRand");
+const outRand = document.getElementById("outRand");
+const calcularAleatorio = (min, max) => {
+  if (min > max) { [min, max] = [max, min]; }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+butRand.onclick = function () {
+  const n1 = parseInt(inRandMin.value);
+  const n2 = parseInt(inRandMax.value);
+  const message = calcularAleatorio(n1, n2)
+  alert(message);
+  outRand.innerHTML = `El número aleatorio es ${message}.`;
+};
+//#endregion
+
+//#region La Caja
+const inCaja = document.getElementById("inCaja");
+const expCaja = document.getElementById("expCaja");
+const outCaja = document.getElementById("outCaja");
+const butCajaMitad = document.getElementById("butCajaMitad");
+const butCajaUltimo = document.getElementById("butCajaUltimo");
+const butCajaInvertida = document.getElementById("butCajaInvertida");
+const butCajaGuiones = document.getElementById("butCajaGuiones");
+const butCajaVocales = document.getElementById("butCajaVocales");
+
+butCajaMitad.onclick = function () {
+  let message = inCaja.value;
+  message = message.slice(0, message.length / 2);
+  expCaja.innerHTML = "La mitad del contenido de la caja:";
+  outCaja.innerHTML = message;
+}
+
+butCajaUltimo.onclick = function () {
+  let message = inCaja.value.match(/.$/);
+  expCaja.innerHTML = "El último carácter de la caja es:";
+  outCaja.innerHTML = message;
+}
+
+butCajaInvertida.onclick = function () {
+  let message = inCaja.value.split('').reverse().join('');
+  expCaja.innerHTML = "El mensaje invertido de la caja es:";
+  outCaja.innerHTML = message;
+}
+
+butCajaGuiones.onclick = function () {
+  let message = inCaja.value.split('').join('-');
+  expCaja.innerHTML = "El mensaje invertido de la caja es:";
+  outCaja.innerHTML = message;
+}
+
+butCajaVocales.onclick = function () {
+  let message = inCaja.value.match(/[aeiou]/gi).length;
+  expCaja.innerHTML = "El número de vocales contenidas dentro de la caja es:";
+  outCaja.innerHTML = message;
+}
+//#endregion
